1a350e9001bf27f85696b64f93eb02bebb0420c8
Mahfuz Zahan Moyeen <mahfuzzm10@gmail.com>
2025-11-16 23:46:18 +0600

    setup file

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='min-h-screen flex justify-center items-center text-5xl font-bold bg-black text-white'>
+            Coming Soon...
+        </div>
+    );
+};
+
+export default Home;
