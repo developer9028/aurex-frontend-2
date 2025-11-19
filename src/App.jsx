4e70f0c3bd0c8bf58cc8ce0b7141b500683517a1
Mahfuz Zahan Moyeen <mahfuzzm10@gmail.com>
2025-11-20 00:15:15 +0600

    create : Ecosystem

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import './App.css'
 import { RouterProvider } from "react-router/dom";
 import router from './router/router'
 
+import SmokeyCursor from "./components/lightswind/smokey-cursor.tsx";
+
 function App() {
 
   return (
     <>
+      {/* SmokeyCursor only for production mode   */}
+      {
+        import.meta.env.PROD && <SmokeyCursor />
+      }
+
       <RouterProvider router={router} />
     </>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { BorderBeam } from "../../components/lightswind/border-beam";
 import Banner from './Banner';
 import OurVisionAndMission from './OurVisionAndMission';
+import Ecosystem from './Ecosystem';
 
 
 const Home = () => {
     return (
-        <div>
+        <div className='mb-20'>
             <Banner />
             <OurVisionAndMission />
-
+            <Ecosystem />
         </div>
     );
 };
