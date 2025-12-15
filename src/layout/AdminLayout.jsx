acac99719ee3bd836186b33ca09620741f8159e7
ruhulamin1398 <ruhulamin010398@gmail.com>
2025-12-15 23:50:04 +0600

    add not conencted

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -25,7 +25,7 @@ const AdminLayout = () => {
     }, [address]);
 
     return (<>{
-        address && address.toLowerCase() !== config.OWNER.toLowerCase() ? (
+        !isConnected || address.toLowerCase() !== config.OWNER.toLowerCase() ? (
             <div className="min-h-screen flex items-center justify-center bg-[#080808]">
                 <h2 className="text-white text-2xl">Access Denied. Please log in as Admin. <Link to="/"><u> click here to go home </u> </Link></h2>
             </div>
