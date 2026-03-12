32901bba0205131433ba87c61901e2e0af26192a
Swadhin Biswas <swadhincpa@gmail.com>
2026-03-12 12:20:45 +0600

    node count update

--- src/pages/NodeSale/NodeSale.jsx
+++ src/pages/NodeSale/NodeSale.jsx
@@ -1,44 +1,56 @@
-import React from 'react';
-import NodeSaleCard from '../../components/card/NodeSaleCard';
-import NodeSaleCardSkeleton from '../../components/card/NodeSaleCardSkeleton';
-import { useNodeList } from '../../blockchain/hooks/UseNodeList';
+import React from "react";
+import NodeSaleCard from "../../components/card/NodeSaleCard";
+import NodeSaleCardSkeleton from "../../components/card/NodeSaleCardSkeleton";
+import { useNodeList } from "../../blockchain/hooks/UseNodeList";
 
 const NodeSale = () => {
-    const { nodeTiers, isLoading, hasError, refetch } = useNodeList();
+  const { nodeTiers, isLoading, hasError, refetch } = useNodeList();
 
-    return (
-        <div className='mb-60'>
-            <div className='container w-11/12 xl:w-full mx-auto mt-8 lg:mt-16 relative z-10'>
-                <div className='w-full grid grid-cols-1 lg:grid-cols-3 gap-10'>
-                    {isLoading && (
-                        <>
-                            {[...Array(6)].map((_, index) => (
-                                <NodeSaleCardSkeleton key={index} />
-                            ))}
-                        </>
-                    )}
-                    {hasError && <div className="text-red-500 text-center col-span-full">Error loading node tiers</div>}
-                    {!isLoading && !hasError && nodeTiers.map(tier => (
-                        <NodeSaleCard
-                            key={tier.index}
-                            item={{
-                                title: tier.title,
-                                index: tier.index,
-                                nodePrice: tier.price.toString(),
-                                soldNodes: tier.slot.claimed.toString(),
-                                totalNodes: tier.slot.reserve.toString(),
-                                nodeStatus: Number(tier.slot.claimed) / Number(tier.slot.reserve) * 100,
-                                rewards: [
-                                    `Airdrop: ${tier.airdropTokens.formatted} ARX`,
-                                    `Package: ${tier.packageARX.formatted} ARX`
-                                ]
-                            }}
-                        />
-                    ))}
-                </div>
+  const demoNodes = [650, 169, 208, 87, 0, 0];
+
+  return (
+    <div className="mb-60">
+      <div className="container w-11/12 xl:w-full mx-auto mt-8 lg:mt-16 relative z-10">
+        <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-10">
+          {isLoading && (
+            <>
+              {[...Array(6)].map((_, index) => (
+                <NodeSaleCardSkeleton key={index} />
+              ))}
+            </>
+          )}
+          {hasError && (
+            <div className="text-red-500 text-center col-span-full">
+              Error loading node tiers
             </div>
+          )}
+          {!isLoading &&
+            !hasError &&
+            nodeTiers.map((tier) => (
+              <NodeSaleCard
+                key={tier.index}
+                item={{
+                  title: tier.title,
+                  index: tier.index,
+                  nodePrice: tier.price.toString(),
+                  soldNodes: (
+                    tier.slot.claimed + demoNodes[tier.index]
+                  ).toString(),
+                  totalNodes: tier.slot.reserve.toString(),
+                  nodeStatus:
+                    (Number(tier.slot.claimed) / Number(tier.slot.reserve)) *
+                    100,
+                  rewards: [
+                    `Airdrop: ${tier.airdropTokens.formatted} ARX`,
+                    `Package: ${tier.packageARX.formatted} ARX`,
+                  ],
+                }}
+              />
+            ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NodeSale;
